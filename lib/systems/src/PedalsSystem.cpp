e9afde9103d317188708b4a498b4035bed2037d1
AgastyaK87 <agastya.kalagarla@gmail.com>
2025-02-24 09:43:00 -0500

    Update PedalsSystem.cpp

--- lib/systems/src/PedalsSystem.cpp
+++ lib/systems/src/PedalsSystem.cpp
@@ -43,10 +43,10 @@ PedalsSystemData_s PedalsSystem::evaluate_pedals(PedalSensorData_s pedals_data, 
 
     out.brake_and_accel_pressed_implausibility_high = _evaluate_brake_and_accel_pressed(pedals_data);
     float accel_percent = (out.accel_is_implausible) ? _accel1_scaled_ : _pedal_percentage(static_cast<float>(accel_1),static_cast<float>(accel_2),_accelParams); 
-    accel_percent = _remove_deadzone(accel_percent, _accelParams.deadzone_margin);
+    out.accel_percent = _remove_deadzone(accel_percent, _accelParams.deadzone_margin);
     out.accel_percent = std::max(out.accel_percent, 0.0f);
     float brake_percent = (out.brake_is_implausible) ? _brake1_scaled_ : _pedal_percentage(static_cast<float>(brake_1),static_cast<float>(brake_2),_brakeParams); 
-    brake_percent = _remove_deadzone(brake_percent, _brakeParams.deadzone_margin);
+    out.brake_percent = _remove_deadzone(brake_percent, _brakeParams.deadzone_margin);
     out.brake_percent = std::max(out.brake_percent, 0.0f);
     bool implausibility = (out.accel_is_implausible || out.brake_and_accel_pressed_implausibility_high || out.brake_is_implausible);
     if (implausibility && (_implausibilityStartTime ==0)){
