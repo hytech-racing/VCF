b8db5eeb8eab774c2d9e9bbda0e2a17cadc8f83c
AgastyaK87 <agastya.kalagarla@gmail.com>
2025-03-06 19:48:13 -0500

    Update test_pedals_main.cpp

--- src/test_pedals_main.cpp
+++ src/test_pedals_main.cpp
@@ -15,24 +15,9 @@
 
 
 
-const PedalsParams accel_params = {
-    .min_pedal_1 = 3445,
-    .min_pedal_2 = 181,
-    .max_pedal_1 = 2000,
-    .max_pedal_2 = 1000,
-    .activation_percentage = 0.05,
-    .min_sensor_pedal_1 = 90,
-    .min_sensor_pedal_2 = 90,
-    .max_sensor_pedal_1 = 4000,
-    .max_sensor_pedal_2 = 4000,
-    .deadzone_margin = .03,
-    .implausibility_margin = IMPLAUSIBILITY_PERCENT,
-    .mechanical_activation_percentage = 0.05
-};
-
-const PedalsParams brake_params = {
-    .min_pedal_1 = 3445,
-    .min_pedal_2 = 181,
+const PedalsParams params = {
+    .min_pedal_1 = 1000,
+    .min_pedal_2 = 2000,
     .max_pedal_1 = 2000,
     .max_pedal_2 = 1000,
     .activation_percentage = 0.05,
@@ -47,14 +32,9 @@ const PedalsParams brake_params = {
 
 
 void setup(){
-<<<<<<< HEAD
     PedalsSystemInstance::create(params, params);
     const int begin_time = 115200;
     Serial.begin(begin_time);
-=======
-    PedalsSystemInstance::create(accel_params, brake_params);
-    Serial.begin(115200);
->>>>>>> 13e3134267e275653c64d3474d2785be812cb22f
     SPI.begin();
     init_adc_task();
 }
@@ -71,33 +51,34 @@ void loop(){
     pedal_sensor_data.brake_2 = ADCsOnVCFInstance::instance().adc_2.data.conversions[BRAKE_2_CHANNEL].conversion;
     
     PedalsSystemData_s data = PedalsSystemInstance::instance().evaluate_pedals(pedal_sensor_data, millis());
-    Serial.printf("Accel 1: %d\n", pedal_sensor_data.accel_1);
-    Serial.printf("Accel 2: %d\n", pedal_sensor_data.accel_2);
-    Serial.printf("Brake 1: %d\n", pedal_sensor_data.brake_1);
-    Serial.printf("Brake 2: %d\n", pedal_sensor_data.brake_2);
-
-    //Serial.printf("Accel Percent: %f\n + Brake Percent: %f\n ", data.accel_percent, data.brake_percent);
-    Serial.printf("Accel Implausible: %d\n", data.accel_is_implausible);
-    Serial.printf("Brake Implausible: %d\n", data.brake_is_implausible);
-    Serial.printf("Brake Pressed: %d\n", data.brake_is_pressed);
-    Serial.printf("Accel Pressed: %d\n", data.accel_is_pressed);
-    Serial.printf("Mech Brake Active: %d\n", data.mech_brake_is_active);
-    Serial.printf("Brake and Accel Implausibility High: %d\n", data.brake_and_accel_pressed_implausibility_high);
-    Serial.printf("Implausibility has exceeded max duration: %d\n", data.implausibility_has_exceeded_max_duration);
-
-    Serial.print("Accel Percent: ");
+    Serial.print("Accel 1: ");
+    Serial.println(pedal_sensor_data.accel_1);
+    Serial.print("Accel 2: ");
+    Serial.println(pedal_sensor_data.accel_2);
+    Serial.print("Brake 1: ");
+    Serial.println(pedal_sensor_data.brake_1);
+    Serial.print("Brake 2: ");
+    Serial.println(pedal_sensor_data.brake_2);
+    Serial.println("Accel Implausible: ");
+    Serial.println(data.accel_is_implausible);
+    Serial.println("Brake Implausible: ");
+    Serial.println(data.brake_is_implausible);
+    Serial.println("Brake Pressed: ");
+    Serial.println(data.brake_is_pressed);
+    Serial.println("Accel Pressed: ");
+    Serial.println(data.accel_is_pressed);
+    Serial.println("Mech Brake Active: ");
+    Serial.println(data.mech_brake_is_active);
+    Serial.println("Brake and Accel Implausibility High: ");
+    Serial.println(data.brake_and_accel_pressed_implausibility_high);
+    Serial.println("Implausibility has exceeded max duration: ");
+    Serial.println(data.implausibility_has_exceeded_max_duration);
+    Serial.println("Accel Percent: ");
     Serial.println(data.accel_percent);
-    Serial.print("Brake Percent: ");
+    Serial.println("Brake Percent: ");
     Serial.println(data.brake_percent);
-<<<<<<< HEAD
     Serial.println("Regen Percent: ");
     Serial.println(data.regen_percent);    
     const int delay_time = 1000;
     delay(delay_time);
-=======
-    Serial.print("Regen Percent: ");
-    Serial.println(data.regen_percent);
-    Serial.println("\n\n");
-    delay(1000);
->>>>>>> 13e3134267e275653c64d3474d2785be812cb22f
 }
