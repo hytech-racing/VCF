c7ad3c0069b8b012230cd601989559948e3aae27
Justin Hwang <justin@goliath.org>
2025-04-12 08:30:21 -0400

    threshold set to 4.3

--- lib/interfaces/src/ACUInterface.cpp
+++ lib/interfaces/src/ACUInterface.cpp
@@ -5,5 +5,5 @@ void ACUInterface::receive_ACU_voltages(const CAN_message_t &can_msg)
     BMS_VOLTAGES_t unpacked_msg;
     Unpack_BMS_VOLTAGES_hytech(&unpacked_msg, can_msg.buf, can_msg.len);
     
-    _voltages_not_critical = HYTECH_low_voltage_ro_fromS(unpacked_msg.low_voltage_ro) < 4.0f;
+    _voltages_not_critical = HYTECH_low_voltage_ro_fromS(unpacked_msg.low_voltage_ro) < 4.3f;
 }
