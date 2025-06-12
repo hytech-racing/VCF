3885b18ca106f47563a98e91617c63d9a63ce66c
skse <nazcar101@gmail.com>
2025-06-11 22:20:00 -0400

    linter fixes

--- lib/interfaces/src/VCRInterface.cpp
+++ lib/interfaces/src/VCRInterface.cpp
@@ -3,7 +3,7 @@
 void VCRInterface::receive_dash_control_data(const CAN_message_t &can_msg)
 {
     DASHBOARD_BUZZER_CONTROL_t unpacked_msg;
-    Unpack_DASHBOARD_BUZZER_CONTROL_hytech(&unpacked_msg, can_msg.buf, can_msg.len);            
+    Unpack_DASHBOARD_BUZZER_CONTROL_hytech(&unpacked_msg, can_msg.buf, can_msg.len);    //NOLINT         
 
     if (unpacked_msg.dash_buzzer_flag) {
         BuzzerController::getInstance().activate(millis());
@@ -20,13 +20,13 @@ void VCRInterface::receive_dash_control_data(const CAN_message_t &can_msg)
 void VCRInterface::receive_vehicle_state_data(const CAN_message_t &can_msg)
 {
     VEHICLE_STATE_t unpacked_msg;
-    Unpack_VEHICLE_STATE_hytech(&unpacked_msg, can_msg.buf, can_msg.len);
+    Unpack_VEHICLE_STATE_hytech(&unpacked_msg, can_msg.buf, can_msg.len); //NOLINT
     _vehicle_state_value = unpacked_msg.drivetrain_state;
 }
 
 void VCRInterface::receive_software_status(const CAN_message_t &can_msg)
 {
     DRIVEBRAIN_STATE_DATA_t unpacked_msg;
-    Unpack_DRIVEBRAIN_STATE_DATA_hytech(&unpacked_msg, can_msg.buf, can_msg.len);
+    Unpack_DRIVEBRAIN_STATE_DATA_hytech(&unpacked_msg, can_msg.buf, can_msg.len); //NOLINT
     _is_db_in_ctrl = unpacked_msg.drivebrain_in_control;
 }
