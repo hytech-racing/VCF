a277af5597e6a40eacb40cc29ffe125df2196a46
Alex Bailey <alexanderbailey@outlook.com>
2025-12-16 21:23:57 -0500

    fixed test_can.cpp to pass GitHub CI checks

--- src/test_can.cpp
+++ src/test_can.cpp
@@ -5,6 +5,13 @@
 FlexCAN_T4<CAN1> TELEM_CAN;
 FlexCAN_T4<CAN2> FAUX_CAN;
 
+const uint32_t CAN_BAUDRATE = 500000;
+
+const uint32_t MSG_ID = 0x11;
+const uint8_t MSG_DATA = 0x45;
+
+const uint32_t DELAY = 10;
+
 void on_recv_telem(const CAN_message_t &msg)
 {
     Serial.println("msg recvd on telem can!");
@@ -13,8 +20,8 @@ void on_recv_telem(const CAN_message_t &msg)
     Serial.print("  EXT: "); Serial.print(msg.flags.extended);
     Serial.print("  LEN: "); Serial.print(msg.len);
     Serial.print(" DATA: ");
-    for ( uint8_t i = 0; i < 8; i++ ) {
-      Serial.print(msg.buf[i]); Serial.print(" ");
+    for (uint8_t byte : msg.buf) {
+      Serial.print(byte); Serial.print(" ");
     }
     Serial.print("  TS: "); Serial.println(msg.timestamp);
 }
@@ -27,8 +34,8 @@ void on_recv_faux(const CAN_message_t &msg)
     Serial.print("  EXT: "); Serial.print(msg.flags.extended);
     Serial.print("  LEN: "); Serial.print(msg.len);
     Serial.print(" DATA: ");
-    for ( uint8_t i = 0; i < 8; i++ ) {
-      Serial.print(msg.buf[i]); Serial.print(" ");
+    for (uint8_t byte : msg.buf) {
+      Serial.print(byte); Serial.print(" ");
     }
     Serial.print("  TS: "); Serial.println(msg.timestamp);
 }
@@ -36,17 +43,17 @@ void on_recv_faux(const CAN_message_t &msg)
 void setup()
 {
     
-    handle_CAN_setup(TELEM_CAN, 500000, &on_recv_telem);
-    handle_CAN_setup(FAUX_CAN, 500000, &on_recv_faux);
+    handle_CAN_setup(TELEM_CAN, CAN_BAUDRATE, &on_recv_telem);
+    handle_CAN_setup(FAUX_CAN, CAN_BAUDRATE, &on_recv_faux);
 }
 
 void loop()
 {
-    delay(10);
+    delay(DELAY);
     CAN_message_t test_msg;
-    test_msg.id = 0x11;
+    test_msg.id = MSG_ID;
     test_msg.len = 1;
-    test_msg.buf[0] = 0x45;
+    test_msg.buf[0] = MSG_DATA;
     TELEM_CAN.write(test_msg);
     FAUX_CAN.write(test_msg);
     // Serial.println("testing");
