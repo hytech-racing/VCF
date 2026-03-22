10bbed7e2436ca801f581abc5c1ee565c9841a1e
Nazar <nazcar101@gmail.com>
2026-03-22 03:12:45 -0400

    updated pedals recal button to make it work properly

--- include/VCF_Constants.h
+++ include/VCF_Constants.h
@@ -17,9 +17,9 @@ namespace VCFInterfaceConstants {
     constexpr int BTN_START_READ = 29; // RTD on schematic
     constexpr int BTN_DATA_READ = 30; // DATA_MARK on schematic
     constexpr int BUZZER_CONTROL_PIN = 32;
-    constexpr int BTN_PRESET_READ = 26; //BUTTON_1 on schematic
+    constexpr int BRIGHTNESS_CONTROL_PIN = 26; //BUTTON_1 on schematic
     constexpr int BUTTON_2 = 27; // BUTTON_2 on schematic
-    constexpr int BRIGHTNESS_CONTROL_PIN = 28; // Currently unused
+    constexpr int BTN_PRESET_READ = 28; // Pedals recal button (brightness control on schematic)
     
     constexpr int NEOPIXEL_CONTROL_PIN = 33;
     constexpr int NEOPIXEL_COUNT = 12; // 12 neopixeles on dashboard
